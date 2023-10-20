721644b056d470142730e07bb0110fdb75237bd0
John Doe <sergey@itikho.com>
2023-10-20 19:28:13 +0300

    done

--- ColorizedDelegate/ViewControllers/MainViewController.swift
+++ ColorizedDelegate/ViewControllers/MainViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate: AnyObject {
+    func saveSettings(redColor: CGFloat, greenColor: CGFloat, blueColor: CGFloat)
+}
+
 final class MainViewController: UIViewController {
+    
+    var red: CGFloat!
+    var green: CGFloat!
+    var blue: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        red = 0.0
+        green = 0.0
+        blue = 0.0
+        view.backgroundColor = UIColor (red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.tempRed = red
+        settingsVC.tempGreen = green
+        settingsVC.tempBlue = blue
+        settingsVC.delegate = self
     }
 
+}
 
+extension MainViewController: SettingsViewControllerDelegate {
+    func saveSettings(redColor: CGFloat, greenColor: CGFloat, blueColor: CGFloat) {
+        red = redColor
+        green = greenColor
+        blue = blueColor
+        view.backgroundColor = UIColor (red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+    }
+    
+    
 }
 
--- ColorizedDelegate/ViewControllers/SettingsViewController.swift
+++ ColorizedDelegate/ViewControllers/SettingsViewController.swift
@@ -8,25 +8,47 @@
 import UIKit
 
 final class SettingsViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
 
+    var tempGreen: CGFloat!
+    var tempRed: CGFloat!
+    var tempBlue: CGFloat!
+    
+    unowned var delegate: SettingsViewControllerDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        redSlider.value = Float(tempRed)
+        greenSlider.value = Float(tempGreen)
+        blueSlider.value = Float(tempBlue)
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func doneButtonPressed() {
+        delegate.saveSettings(redColor: CGFloat(redSlider.value), greenColor: CGFloat(greenSlider.value), blueColor: CGFloat(blueSlider.value))
         dismiss(animated: true)
     }
     
+    @IBAction func sliderMoved() {
+        setupView()
+    }
+    
+    private func setupView() {
+        colorView.layer.cornerRadius = 10
+        redLabel.text = String(format: "%.1f", redSlider.value)
+        greenLabel.text = String(format: "%.1f", greenSlider.value)
+        blueLabel.text = String(format: "%.1f", blueSlider.value)
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        colorView.backgroundColor = UIColor (red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
 }
